120b330eef3ee8627bc7da15222db797e16537e3
Kaben Nanlohy <nanlohy@msu.edu>
2005-04-14 13:03:16 +0000

    * Added some comments and removed some debug code.

--- source/python/AvidaGui2/pyHardwareTracer.py
+++ source/python/AvidaGui2/pyHardwareTracer.py
@@ -31,16 +31,21 @@ class pyHardwareTracer(pyHardwareTracerBase):
     backup_environment = None
     original_resource_count = None
     backup_usage = None
+    # Backup test cpu if we need to use resources.
     if should_use_resources:
       backup_environment, original_resource_count, backup_usage = self.backupTestCPU(environment)
 
+    # Build the test info for tracing hardware.
     test_info = cCPUTestInfo()
     test_info.TestThreads()
+    # Build storage for hardware trace info.
     self.m_hardware_trace = pyHardwareCPUTrace()
     test_info.SetTraceExecution(self)
+    # Trace the genotype's execution.
     cTestCPU.TestGenome(test_info, analyze_genotype.GetGenome())
-
+    # Record some of the genotype's statistics.
     analyze_genotype.Recalculate()
     self.m_hardware_trace.recordGenotypeSummary(analyze_genotype)
 
+    # Restore test cpu if we need to use resources.
     if should_use_resources: self.restoreTestCPU(backup_environment, original_resource_count, backup_usage)
