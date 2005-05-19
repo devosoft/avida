8db13ae662d9208d65c2b4abd1bbf27b91fab974
Jeff Clune <jeffclune@cornell.edu>
2005-05-19 23:16:02 +0000

    You can now choose different colors for your plot lines in analyze mode.

--- source/python/AvidaGui2/pyOneAna_GraphCtrl.py
+++ source/python/AvidaGui2/pyOneAna_GraphCtrl.py
@@ -53,8 +53,15 @@ class pyOneAna_GraphCtrl(pyOneAna_GraphView):
       self.m_combo_box_2.insertItem(entry[0])
 
     # set up the plot line color options
-    for entry in self.m_avida_stats_interface.m_entries:
-      self.m_combo_box_1.insertItem(entry[0])
+    self.m_Red = ['red', Qt.red]
+    self.m_Blue = ['blue', Qt.blue]
+    self.m_Green = ['green', Qt.green]
+    self.m_Colors = [self.m_Red, self.m_Blue, self.m_Green]
+    self.m_combo_box_1_color.clear()
+    self.m_combo_box_2_color.clear()
+    for color in self.m_Colors:
+      self.m_combo_box_1_color.insertItem(color[0])
+      self.m_combo_box_2_color.insertItem(color[0])
 
     
     # connect combo boxes to signal
@@ -62,6 +69,10 @@ class pyOneAna_GraphCtrl(pyOneAna_GraphView):
       self.m_combo_box_1, SIGNAL("activated(int)"), self.modeActivatedSlot)
     self.connect(
       self.m_combo_box_2, SIGNAL("activated(int)"), self.modeActivatedSlot)
+    self.connect(
+      self.m_combo_box_1_color, SIGNAL("activated(int)"), self.modeActivatedSlot)
+    self.connect(
+      self.m_combo_box_2_color, SIGNAL("activated(int)"), self.modeActivatedSlot)
   
 
     self.m_x_array = zeros(2, Float)
@@ -70,11 +81,15 @@ class pyOneAna_GraphCtrl(pyOneAna_GraphView):
     self.m_graph_ctrl.setAxisTitle(QwtPlot.xBottom, "Time (updates)")
     self.m_graph_ctrl.setAxisAutoScale(QwtPlot.xBottom)
 
-    # Start the left with second graph mode -- "Average Fitness".
+    # Start the left with second graph mode -- "Average Fitness"
     self.m_combo_box_1.setCurrentItem(2)
 
     # Start the right with zeroth mode -- "None"
     self.m_combo_box_2.setCurrentItem(0)
+
+    # Start the left with default of red
+    self.m_combo_box_1_color.setCurrentItem(0)
+    self.m_combo_box_2_color.setCurrentItem(2)
 
     self.modeActivatedSlot(self.m_combo_box_1.currentItem()) 
 
@@ -83,7 +98,6 @@ class pyOneAna_GraphCtrl(pyOneAna_GraphView):
       self.printGraphSlot)
 
   def load(self, filename, colx, coly):
-    print "pyOneAna_GraphCtrl.load - loading from file"
     init_file = cInitFile(cString('default.workspace/freezer/2k.full/' + filename))
     init_file.Load()
     init_file.Compress()
@@ -98,11 +112,7 @@ class pyOneAna_GraphCtrl(pyOneAna_GraphView):
     return x_array, y_array
   
   def modeActivatedSlot(self, index):
-    self.m_graph_ctrl.setTitle(self.m_avida_stats_interface.m_entries[0][0])
     self.m_graph_ctrl.clear()
-    self.m_graph_ctrl.enableYRightAxis(False)
-
- 
 
     if self.m_combo_box_1.currentItem() or self.m_combo_box_2.currentItem():
 
@@ -118,9 +128,11 @@ class pyOneAna_GraphCtrl(pyOneAna_GraphView):
         )
         self.m_graph_ctrl.m_curve_1 = self.m_graph_ctrl.insertCurve(self.m_avida_stats_interface.m_entries[index_1][0])
         self.m_graph_ctrl.setCurveData(self.m_graph_ctrl.m_curve_1, self.m_curve_1_arrays[0], self.m_curve_1_arrays[1])
-        self.m_graph_ctrl.setCurvePen(self.m_graph_ctrl.m_curve_1, QPen(Qt.red))
+        self.m_graph_ctrl.setCurvePen(self.m_graph_ctrl.m_curve_1, QPen(self.m_Colors[self.m_combo_box_1_color.currentItem()][1]))  
         self.m_graph_ctrl.setCurveYAxis(self.m_graph_ctrl.m_curve_1, QwtPlot.yLeft)
-        self.m_graph_ctrl.setTitle(self.m_avida_stats_interface.m_entries[index_1][0])
+        if not self.m_combo_box_2.currentItem():
+          self.m_graph_ctrl.enableYRightAxis(False)
+          self.m_graph_ctrl.setTitle(self.m_avida_stats_interface.m_entries[index_1][0])
       else:
         self.m_graph_ctrl.enableYLeftAxis(False)
 
@@ -137,16 +149,17 @@ class pyOneAna_GraphCtrl(pyOneAna_GraphView):
         )
         self.m_graph_ctrl.m_curve_2 = self.m_graph_ctrl.insertCurve(self.m_avida_stats_interface.m_entries[index_2][0])
         self.m_graph_ctrl.setCurveData(self.m_graph_ctrl.m_curve_2, self.m_curve_2_arrays[0], self.m_curve_2_arrays[1])
-        self.m_graph_ctrl.setCurvePen(self.m_graph_ctrl.m_curve_2, QPen(Qt.green))
+        self.m_graph_ctrl.setCurvePen(self.m_graph_ctrl.m_curve_2, QPen(self.m_Colors[self.m_combo_box_2_color.currentItem()][1]))
         self.m_graph_ctrl.setCurveYAxis(self.m_graph_ctrl.m_curve_2, QwtPlot.yRight)
-        self.m_graph_ctrl.setTitle(self.m_avida_stats_interface.m_entries[index_2][0])
+        if not self.m_combo_box_1.currentItem():
+          self.m_graph_ctrl.setTitle(self.m_avida_stats_interface.m_entries[index_2][0])
 
 
       self.m_graph_ctrl.setAxisAutoScale(QwtPlot.xBottom)
 
       if self.m_combo_box_1.currentItem() and self.m_combo_box_2.currentItem():    
-        self.m_graph_ctrl.setTitle(self.m_avida_stats_interface.m_entries[self.m_combo_box_1.currentItem()][0]+ ' (red) and ' + self.m_avida_stats_interface.m_entries[self.m_combo_box_2.currentItem()][0]+ 
-' (green)')
+        self.m_graph_ctrl.setTitle(self.m_avida_stats_interface.m_entries[self.m_combo_box_1.currentItem()][0]+ ' (' + self.m_Colors[self.m_combo_box_1_color.currentItem()][0] \
+        + ') and ' + self.m_avida_stats_interface.m_entries[self.m_combo_box_2.currentItem()][0]+ ' (' +  self.m_Colors[self.m_combo_box_2_color.currentItem()][0] +')')
         bounding_rect_1 = self.m_graph_ctrl.curve(self.m_graph_ctrl.m_curve_1).boundingRect()
         bounding_rect_2 = self.m_graph_ctrl.curve(self.m_graph_ctrl.m_curve_2).boundingRect()
         bounding_rect = bounding_rect_1.unite(bounding_rect_2)
@@ -158,6 +171,7 @@ class pyOneAna_GraphCtrl(pyOneAna_GraphView):
       self.m_graph_ctrl.m_zoomer.setZoomBase(bounding_rect)
   
     else:   # goes with '   if self.m_combo_box_1.currentItem() or self.m_combo_box_2.currentItem():'
+       self.m_graph_ctrl.setTitle(self.m_avida_stats_interface.m_entries[0][0])
        self.m_graph_ctrl.setAxisTitle(QwtPlot.yLeft, self.m_avida_stats_interface.m_entries[0][0])
 
     self.m_graph_ctrl.replot()
