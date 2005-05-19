a2243065887fddd3ae98b6db2b21e74b67e39fc1
Jeff Clune <jeffclune@cornell.edu>
2005-05-19 04:36:10 +0000

    You can now plot two lines on the same graph in analyze mode. I also set up pyOneAna_PetriDishCtrl.py to setText the name of the population (to hook up when drag and drop is available).

--- source/python/AvidaGui2/pyOneAna_GraphCtrl.py
+++ source/python/AvidaGui2/pyOneAna_GraphCtrl.py
@@ -41,12 +41,24 @@ class pyOneAna_GraphCtrl(pyOneAna_GraphView):
     self.m_avida = None
 
     self.m_graph_ctrl.construct(self.m_session_mdl)
-    self.m_combo_box.clear()
-    self.m_combo_box.setInsertionPolicy(QComboBox.AtBottom)
+    self.m_combo_box_1.clear()
+    self.m_combo_box_2.clear()
+    self.m_combo_box_1.setInsertionPolicy(QComboBox.AtBottom)
+    self.m_combo_box_2.setInsertionPolicy(QComboBox.AtBottom)
     for entry in self.m_avida_stats_interface.m_entries:
-      self.m_combo_box.insertItem(entry[0])
+      self.m_combo_box_1.insertItem(entry[0])
+    for entry in self.m_avida_stats_interface.m_entries:
+      self.m_combo_box_2.insertItem(entry[0])
+    
+   
+
+    # the first int is the entry from the combo box (which graph the user wants)
+    # the second int tells it which combo box is set to that
     self.connect(
-      self.m_combo_box, SIGNAL("activated(int)"), self.modeActivatedSlot)
+      self.m_combo_box_1, SIGNAL("activated(int)"), self.modeActivatedSlot)
+    self.connect(
+      self.m_combo_box_2, SIGNAL("activated(int)"), self.modeActivatedSlot)
+  
 
     self.m_x_array = zeros(2, Float)
     self.m_y_array = zeros(2, Float)
@@ -54,9 +66,13 @@ class pyOneAna_GraphCtrl(pyOneAna_GraphView):
     self.m_graph_ctrl.setAxisTitle(QwtPlot.xBottom, "Time (updates)")
     self.m_graph_ctrl.setAxisAutoScale(QwtPlot.xBottom)
 
-    # Start with second graph mode -- "Average Fitness".
-    self.m_combo_box.setCurrentItem(2)
-    self.modeActivatedSlot(self.m_combo_box.currentItem())
+    # Start the left with second graph mode -- "Average Fitness".
+    self.m_combo_box_1.setCurrentItem(2)
+
+    # Start the right with zeroth mode -- "None"
+    self.m_combo_box_2.setCurrentItem(0)
+
+    self.modeActivatedSlot(self.m_combo_box_1.currentItem()) 
 
     self.connect(
       self.m_session_mdl.m_session_mdtr, PYSIGNAL("printGraphSig"),
@@ -75,53 +91,69 @@ class pyOneAna_GraphCtrl(pyOneAna_GraphView):
       line = init_file.GetLine(line_id)
       x_array[line_id] = line.GetWord(colx - 1).AsDouble()
       y_array[line_id] = line.GetWord(coly - 1).AsDouble()
-##
-#      self.m_y_array_2[line_id] = line.GetWord(2).AsDouble()
-##
     return x_array, y_array
-
+  
   def modeActivatedSlot(self, index):
-    self.m_graph_ctrl.setTitle(self.m_avida_stats_interface.m_entries[index][0])
+    self.m_graph_ctrl.setTitle(self.m_avida_stats_interface.m_entries[0][0])
     self.m_graph_ctrl.clear()
-    if index:
+    self.m_graph_ctrl.enableYRightAxis(False)
 
-      fake_index_1 = 1
-      fake_index_2 = 2
+ 
 
-      self.m_graph_ctrl.setAxisTitle(QwtPlot.yLeft, self.m_avida_stats_interface.m_entries[fake_index_1][0])
-      self.m_graph_ctrl.setAxisTitle(QwtPlot.yRight, self.m_avida_stats_interface.m_entries[fake_index_2][0])
+    if self.m_combo_box_1.currentItem() or self.m_combo_box_2.currentItem():
+
+      if self.m_combo_box_1.currentItem():
+        index_1 = self.m_combo_box_1.currentItem()
+        self.m_graph_ctrl.setAxisTitle(QwtPlot.yLeft, self.m_avida_stats_interface.m_entries[index_1][0])
+        self.m_graph_ctrl.setAxisAutoScale(QwtPlot.yLeft)
+        self.m_curve_1_arrays = self.load(
+            self.m_avida_stats_interface.m_entries[index_1][1],
+            1,
+            self.m_avida_stats_interface.m_entries[index_1][2]
+        )
+        self.m_graph_ctrl.m_curve_1 = self.m_graph_ctrl.insertCurve(self.m_avida_stats_interface.m_entries[index_1][0])
+        self.m_graph_ctrl.setCurveData(self.m_graph_ctrl.m_curve_1, self.m_curve_1_arrays[0], self.m_curve_1_arrays[1])
+        self.m_graph_ctrl.setCurvePen(self.m_graph_ctrl.m_curve_1, QPen(Qt.red))
+        self.m_graph_ctrl.setCurveYAxis(self.m_graph_ctrl.m_curve_1, QwtPlot.yLeft)
+        self.m_graph_ctrl.setTitle(self.m_avida_stats_interface.m_entries[index_1][0])
+      else:
+        self.m_graph_ctrl.enableYLeftAxis(False)
+
+
+      if self.m_combo_box_2.currentItem():
+        index_2 = self.m_combo_box_2.currentItem()
+        self.m_graph_ctrl.setAxisTitle(QwtPlot.yRight, self.m_avida_stats_interface.m_entries[index_2][0])
+        self.m_graph_ctrl.enableYRightAxis(True)      
+        self.m_graph_ctrl.setAxisAutoScale(QwtPlot.yRight)
+        self.m_curve_2_arrays = self.load(
+            self.m_avida_stats_interface.m_entries[index_2][1],
+            1,
+            self.m_avida_stats_interface.m_entries[index_2][2]
+        )
+        self.m_graph_ctrl.m_curve_2 = self.m_graph_ctrl.insertCurve(self.m_avida_stats_interface.m_entries[index_2][0])
+        self.m_graph_ctrl.setCurveData(self.m_graph_ctrl.m_curve_2, self.m_curve_2_arrays[0], self.m_curve_2_arrays[1])
+        self.m_graph_ctrl.setCurvePen(self.m_graph_ctrl.m_curve_2, QPen(Qt.green))
+        self.m_graph_ctrl.setCurveYAxis(self.m_graph_ctrl.m_curve_2, QwtPlot.yRight)
+        self.m_graph_ctrl.setTitle(self.m_avida_stats_interface.m_entries[index_2][0])
+
 
       self.m_graph_ctrl.setAxisAutoScale(QwtPlot.xBottom)
-      self.m_graph_ctrl.setAxisAutoScale(QwtPlot.yLeft)
-      self.m_graph_ctrl.setAxisAutoScale(QwtPlot.yRight)
 
-      self.m_curve_1_arrays = self.load(
-        self.m_avida_stats_interface.m_entries[fake_index_1][1],
-        1,
-        self.m_avida_stats_interface.m_entries[fake_index_1][2]
-      )
-      self.m_curve_2_arrays = self.load(
-        self.m_avida_stats_interface.m_entries[fake_index_2][1],
-        1,
-        self.m_avida_stats_interface.m_entries[fake_index_2][2]
-      )
+      if self.m_combo_box_1.currentItem() and self.m_combo_box_2.currentItem():    
+        self.m_graph_ctrl.setTitle(self.m_avida_stats_interface.m_entries[self.m_combo_box_1.currentItem()][0]+ ' (red) and ' + self.m_avida_stats_interface.m_entries[self.m_combo_box_2.currentItem()][0]+ 
+' (green)')
+        bounding_rect_1 = self.m_graph_ctrl.curve(self.m_graph_ctrl.m_curve_1).boundingRect()
+        bounding_rect_2 = self.m_graph_ctrl.curve(self.m_graph_ctrl.m_curve_2).boundingRect()
+        bounding_rect = bounding_rect_1.unite(bounding_rect_2)
+      elif self.m_combo_box_1.currentItem():
+        bounding_rect = self.m_graph_ctrl.curve(self.m_graph_ctrl.m_curve_1).boundingRect()
+      else:
+        bounding_rect = self.m_graph_ctrl.curve(self.m_graph_ctrl.m_curve_2).boundingRect()
 
-      self.m_graph_ctrl.m_curve_1 = self.m_graph_ctrl.insertCurve(self.m_avida_stats_interface.m_entries[fake_index_1][0])
-      self.m_graph_ctrl.m_curve_2 = self.m_graph_ctrl.insertCurve(self.m_avida_stats_interface.m_entries[fake_index_2][0])
-
-      self.m_graph_ctrl.setCurveData(self.m_graph_ctrl.m_curve_1, self.m_curve_1_arrays[0], self.m_curve_1_arrays[1])
-      self.m_graph_ctrl.setCurveData(self.m_graph_ctrl.m_curve_2, self.m_curve_2_arrays[0], self.m_curve_2_arrays[1])
-
-      self.m_graph_ctrl.setCurvePen(self.m_graph_ctrl.m_curve_1, QPen(Qt.red))
-      self.m_graph_ctrl.setCurvePen(self.m_graph_ctrl.m_curve_2, QPen(Qt.green))
-
-      self.m_graph_ctrl.setCurveYAxis(self.m_graph_ctrl.m_curve_1, QwtPlot.yLeft)
-      self.m_graph_ctrl.setCurveYAxis(self.m_graph_ctrl.m_curve_2, QwtPlot.yRight)
-
-      bounding_rect_1 = self.m_graph_ctrl.curve(self.m_graph_ctrl.m_curve_1).boundingRect()
-      bounding_rect_2 = self.m_graph_ctrl.curve(self.m_graph_ctrl.m_curve_2).boundingRect()
-      bounding_rect = bounding_rect_1.unite(bounding_rect_2)
       self.m_graph_ctrl.m_zoomer.setZoomBase(bounding_rect)
+  
+    else:   # goes with '   if self.m_combo_box_1.currentItem() or self.m_combo_box_2.currentItem():'
+       self.m_graph_ctrl.setAxisTitle(QwtPlot.yLeft, self.m_avida_stats_interface.m_entries[0][0])
 
     self.m_graph_ctrl.replot()
       
@@ -134,53 +166,4 @@ class pyOneAna_GraphCtrl(pyOneAna_GraphView):
       self.m_graph_ctrl.printPlot(printer, filter)
 
 
-  def avidaUpdatedSlot(self):
-    if self.m_combo_box.currentItem():
-      fake_index_1 = 1
-      fake_index_2 = 2
-
-      self.m_curve_1_arrays[0] = concatenate(
-        (self.m_x_array, [self.m_avida.m_population.GetStats().GetUpdate()]),
-        1
-      )
-      self.m_curve_2_arrays[0] = concatenate(
-        (self.m_x_array, [self.m_avida.m_population.GetStats().GetUpdate()]),
-        1
-      )
-
-      self.m_curve_1_arrays[1] = concatenate(
-        (self.m_curve_1_arrays[1],
-          [self.m_avida_stats_interface.getValue(
-            fake_index_1,
-            self.m_avida.m_population.GetStats()
-          )]
-        ),
-        1
-      )
-      self.m_curve_2_arrays[1] = concatenate(
-        (self.m_curve_2_arrays[1],
-          [self.m_avida_stats_interface.getValue(
-            fake_index_2,
-            self.m_avida.m_population.GetStats()
-          )]
-        ),
-        1
-      )
-
-      if hasattr(self.m_graph_ctrl, "m_curve_1") and hasattr(self.m_graph_ctrl, "m_curve_2"):
-        self.m_graph_ctrl.setCurveData(self.m_graph_ctrl.m_curve_1, self.m_curve_1_arrays[0], self.m_curve_1_arrays[1])
-        self.m_graph_ctrl.setCurveData(self.m_graph_ctrl.m_curve_2, self.m_curve_2_arrays[0], self.m_curve_2_arrays[1])
-
-        # Quick hack: Cause the zoomer to limit zooming-out to the
-        # boundaries of the displayed curve.
-        bounding_rect_1 = self.m_graph_ctrl.curve(self.m_graph_ctrl.m_curve_1).boundingRect()
-        bounding_rect_2 = self.m_graph_ctrl.curve(self.m_graph_ctrl.m_curve_2).boundingRect()
-        bounding_rect = bounding_rect_1.unite(bounding_rect_2)
-        self.m_graph_ctrl.m_zoomer.setZoomBase(bounding_rect)
-        # Quick hack: If the user has zoomed-in on or -out from the graph,
-        # autoscaling will have been disabled. This reenables it.
-        self.m_graph_ctrl.setAxisAutoScale(QwtPlot.xBottom)
-        self.m_graph_ctrl.setAxisAutoScale(QwtPlot.yLeft)
-        self.m_graph_ctrl.setAxisAutoScale(QwtPlot.yRight)
-        self.m_graph_ctrl.replot()
 
--- source/python/AvidaGui2/pyOneAna_PetriDishCtrl.py
+++ source/python/AvidaGui2/pyOneAna_PetriDishCtrl.py
@@ -8,3 +8,4 @@ class pyOneAna_PetriDishCtrl(pyOneAna_PetriDishView):
 
   def __init__(self,parent = None,name = None,fl = 0):
     pyOneAna_PetriDishView.__init__(self,parent,name,fl)
+    self.m_one_ana_pop_name.setText('A Relatively Boring Population')
