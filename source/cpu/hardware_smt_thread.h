d6038a271f2df3369d5de86749f34db3f69f76d7
David M. Bryson <david@programerror.com>
2005-06-04 19:16:19 +0000

    Begin adding new hardware classes.  Added tInstLib template.

---
+++ source/cpu/hardware_smt_thread.h
@@ -0,0 +1,73 @@
+/*
+ *  hardware_smt_thread.h
+ *  Avida2
+ *
+ *  Created by David on 6/4/05.
+ *  Copyright 2005 Michigan State University. All rights reserved.
+ *
+ */
+
+#ifndef HARDWARE_SMT_THREAD_HH
+#define HARDWARE_SMT_THREAD_HH
+
+#include <iostream>
+
+#ifndef CODE_LABEL_HH
+#include "code_label.hh"
+#endif
+#ifndef CPU_STACK_HH
+#include "cpu_stack.hh"
+#endif
+#ifndef FOURSTACK_HEAD_HH
+#include "4stack_head.hh"
+#endif
+#ifndef HARDWARE_4STACK_CONSTANTS_HH
+#include "hardware_4stack_constants.hh"
+#endif
+#ifndef TBUFFER_HH
+#include "tBuffer.hh"
+#endif
+
+/**
+* This class is needed to run several threads on a single genome.
+ *
+ * @see cCPUStack, c4StackHead, cHardwareSMT
+ **/
+
+class c4StackHead; // aggregate
+class cCodeLabel; // aggregate
+class cCPUStack; // aggregate
+class cHardwareBase;
+class cInjectGenotype;
+template <class T> class tBuffer; // aggregate
+
+struct cHardwareSMT_Thread {
+private:
+  int id;
+public:
+  c4StackHead heads[NUM_HEADS];
+  UCHAR cur_head;
+  cCPUStack local_stacks[NUM_LOCAL_STACKS];
+	
+  bool advance_ip;         // Should the IP advance after this instruction?
+  cCodeLabel read_label;
+  cCodeLabel next_label;
+  // If this thread was spawned by Inject, this will point to the genotype 
+  // of the parasite running the thread.  Otherwise, it will be NULL.
+  cInjectGenotype* owner;
+public:
+		cHardwareSMT_Thread(cHardwareBase * in_hardware=NULL, int _id=-1);
+  cHardwareSMT_Thread(const cHardwareSMT_Thread & in_thread, int _id=-1);
+  ~cHardwareSMT_Thread();
+	
+  void operator=(const cHardwareSMT_Thread & in_thread);
+	
+  void Reset(cHardwareBase * in_hardware, int _id);
+  int GetID() const { return id; }
+  void SetID(int _id) { id = _id; }
+	
+  void SaveState(std::ostream & fp);
+  void LoadState(std::istream & fp);
+};
+
+#endif
---
+++ source/cpu/tInstLib.h
@@ -0,0 +1,70 @@
+/*
+ *  tInstLib.h
+ *  Avida2
+ *
+ *  Created by David on 6/4/05.
+ *  Copyright 2005 Michigan State University. All rights reserved.
+ *
+ */
+
+#ifndef STRING_HH
+#include "string.hh"
+#endif
+#ifndef INST_LIB_BASE_HH
+#include "inst_lib_base.hh"
+#endif
+
+class cInstruction;
+
+template <class MethodType> class tInstLib : public cInstLibBase {
+  const size_t m_nopmods_array_size;
+  const size_t m_function_array_size;
+  cString *m_nopmod_names;
+  cString *m_function_names;
+  const int *m_nopmods;
+  MethodType *m_functions;
+  static const cInstruction inst_error;
+  static const cInstruction inst_default;
+public:
+		tInstLib(
+									 size_t nopmod_array_size,
+									 size_t function_array_size,
+									 cString *nopmod_names,
+									 cString *function_names,
+									 const int *nopmods,
+									 MethodType *functions
+									 ):m_nopmods_array_size(nopmod_array_size),
+    m_function_array_size(function_array_size),
+    m_nopmod_names(nopmod_names),
+    m_function_names(function_names),
+    m_nopmods(nopmods),
+    m_functions(functions)
+  {}
+  MethodType *GetFunctions(void){ return m_functions; } 
+  const cString &GetName(const unsigned int id) {
+    assert(id < m_function_array_size);
+    return m_function_names[id];
+  }
+  const cString &GetNopName(const unsigned int id) {
+    assert(id < m_nopmods_array_size);
+    return m_nopmod_names[id];
+  }
+  int GetNopMod(const unsigned int id){
+    assert(id < m_nopmods_array_size);
+    return m_nopmods[id];
+  }
+  int GetNopMod(const cInstruction & inst){
+    return GetNopMod(inst.GetOp());
+  }
+  int GetSize(){ return m_function_array_size; }
+  int GetNumNops(){ return m_nopmods_array_size; }
+  cInstruction GetInst(const cString & in_name){
+    for (unsigned int i = 0; i < m_function_array_size; i++) {
+      if (m_function_names[i] == in_name) return cInstruction(i);
+    }
+    return tInstLib::GetInstError();
+  }
+  const cInstruction & GetInstDefault(){ return inst_default; }
+  const cInstruction & GetInstError(){ return inst_error; }
+};
+
