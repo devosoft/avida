068332125b077445f5c98c9c894872458ac87c10
Kaben Nanlohy <nanlohy@msu.edu>
2006-05-03 04:45:23 +0000

    Oops, forgot to add unit test suite driver for cInitFile-unit-tests.

---
+++ source/testsuites/cInitFile-unit-tests.cc
@@ -0,0 +1,16 @@
+#include "cDataEntry.h"
+#include "cFile.h"
+#include "cInitFile.h"
+#include "cRandom.h"
+#include "cString.h"
+#include "cTemplateTests.h"
+
+#include <boost/detail/lightweight_test.hpp>
+
+int main() {
+
+  cInitFile::UnitTests(true);
+
+  return boost::report_errors();
+}
+
