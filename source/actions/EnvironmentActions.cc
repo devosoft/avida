766b30b32bc8b5811987ea04a9c917b33fad15c7
David M. Bryson <david@programerror.com>
2006-08-19 21:29:36 +0000

    Transition various resource events into the actions framework.

--- source/actions/EnvironmentActions.cc
+++ source/actions/EnvironmentActions.cc
@@ -11,8 +11,140 @@
 
 #include "cAction.h"
 #include "cActionLibrary.h"
+#include "cEnvironment.h"
+#include "cPopulation.h"
+#include "cResource.h"
+#include "cStats.h"
+#include "cWorld.h"
+
+class cActionInjectResource : public cAction
+{
+private:
+  cString m_res_name;
+  double m_res_count;
+  
+public:
+  cActionInjectResource(cWorld* world, const cString& args) : cAction(world, args), m_res_name(""), m_res_count(0.0)
+  {
+    cString largs(args);
+    if (largs.GetSize()) m_res_name = largs.PopWord();
+    if (largs.GetSize()) m_res_count = largs.PopWord().AsDouble();
+  }
+
+  const cString GetDescription() { return "InjectResource <string res_name> <double res_count>"; }
+
+  void Process(cAvidaContext& ctx)
+  {
+    cResource* res = m_world->GetEnvironment().GetResourceLib().GetResource(m_res_name);
+    if (res != NULL) m_world->GetPopulation().UpdateResource(res->GetID(), m_res_count);
+  }
+};
+
+
+/*
+ Inject (add) a specified amount of a specified resource, scaled by
+ the current average merit divided by the average time slice.
+*/
+class cActionInjectScaledResource : public cAction
+{
+private:
+  cString m_res_name;
+  double m_res_count;
+  
+public:
+  cActionInjectScaledResource(cWorld* world, const cString& args) : cAction(world, args), m_res_name(""), m_res_count(0.0)
+  {
+    cString largs(args);
+    if (largs.GetSize()) m_res_name = largs.PopWord();
+    if (largs.GetSize()) m_res_count = largs.PopWord().AsDouble();
+  }
+  
+  const cString GetDescription() { return "InjectScaledResource <string res_name> <double res_count>"; }
+  
+  void Process(cAvidaContext& ctx)
+  {
+    double ave_merit = m_world->GetStats().SumMerit().Average();
+    if (ave_merit <= 0.0) ave_merit = 1.0; // make sure that we don't get NAN's or negative numbers
+    ave_merit /= m_world->GetConfig().AVE_TIME_SLICE.Get();
+
+    cResource* res = m_world->GetEnvironment().GetResourceLib().GetResource(m_res_name);
+    if (res != NULL) m_world->GetPopulation().UpdateResource(res->GetID(), (m_res_count / ave_merit));
+  }
+};
+
+
+/*
+ Removes a specified percentage of a specified resource, scaled by
+ the current average merit divided by the average time slice.
+ */
+class cActionOutflowScaledResource : public cAction
+{
+private:
+  cString m_res_name;
+  double m_res_percent;
+  
+public:
+  cActionOutflowScaledResource(cWorld* world, const cString& args) : cAction(world, args), m_res_name(""), m_res_percent(0.0)
+  {
+    cString largs(args);
+    if (largs.GetSize()) m_res_name = largs.PopWord();
+    if (largs.GetSize()) m_res_percent = largs.PopWord().AsDouble();
+  }
+  
+  const cString GetDescription() { return "OutflowScaledResource <string res_name> <double res_count>"; }
+  
+  void Process(cAvidaContext& ctx)
+  {
+    double ave_merit = m_world->GetStats().SumMerit().Average();
+    if (ave_merit <= 0.0) ave_merit = 1.0; // make sure that we don't get NAN's or negative numbers
+    ave_merit /= m_world->GetConfig().AVE_TIME_SLICE.Get();
+
+    cResource* res = m_world->GetEnvironment().GetResourceLib().GetResource(m_res_name);
+    
+    double res_level = m_world->GetPopulation().GetResource(res->GetID());
+    double scaled_perc = 1 / (1 + ave_merit * (1 - m_res_percent) / m_res_percent);
+    res_level -= res_level * scaled_perc;
+    
+    if (res != NULL) m_world->GetPopulation().UpdateResource(res->GetID(), res_level);
+  }
+};
+
+
+class cActionSetResource : public cAction
+{
+private:
+  cString m_res_name;
+  double m_res_count;
+  
+public:
+  cActionSetResource(cWorld* world, const cString& args) : cAction(world, args), m_res_name(""), m_res_count(0.0)
+  {
+    cString largs(args);
+    if (largs.GetSize()) m_res_name = largs.PopWord();
+    if (largs.GetSize()) m_res_count = largs.PopWord().AsDouble();
+  }
+  
+  const cString GetDescription() { return "SetResource <string res_name> <double res_count>"; }
+  
+  void Process(cAvidaContext& ctx)
+  {
+    cResource* res = m_world->GetEnvironment().GetResourceLib().GetResource(m_res_name);
+    if (res != NULL) m_world->GetPopulation().SetResource(res->GetID(), m_res_count);
+  }
+};
+
 
 
 void RegisterEnvironmentActions(cActionLibrary* action_lib)
 {
+  action_lib->Register<cActionInjectResource>("InjectResource");
+  action_lib->Register<cActionInjectScaledResource>("InjectScaledResource");
+  action_lib->Register<cActionOutflowScaledResource>("OutflowScaledResource");
+  action_lib->Register<cActionSetResource>("SetResource");
+  
+  // @DMB - The following actions are DEPRECATED aliases - These will be removed in 2.7.
+  action_lib->Register<cActionInjectResource>("inject_resource");
+  action_lib->Register<cActionInjectScaledResource>("inject_scaled_resource");
+  action_lib->Register<cActionOutflowScaledResource>("outflow_scaled_resource");
+  action_lib->Register<cActionSetResource>("set_resource");
 }
