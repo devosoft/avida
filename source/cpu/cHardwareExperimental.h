61a711ab65db44f11b278dfe09bbb20252baf80e
David M. Bryson <david@programerror.com>
2007-02-10 18:24:30 +0000

    Adjust experimental hardware to 5 registers/heads/nops.

--- source/cpu/cHardwareExperimental.h
+++ source/cpu/cHardwareExperimental.h
@@ -68,10 +68,10 @@ public:
 
 protected:
   // --------  Structure Constants  --------
-  static const int NUM_REGISTERS = 4;
+  static const int NUM_REGISTERS = 5;
   static const int NUM_HEADS = nHardware::NUM_HEADS >= NUM_REGISTERS ? nHardware::NUM_HEADS : NUM_REGISTERS;
   enum tRegisters { REG_AX = 0, REG_BX, REG_CX, REG_DX, REG_EX, REG_FX };
-  static const int NUM_NOPS = 4;
+  static const int NUM_NOPS = 5;
   
   // --------  Data Structures  --------
   struct cLocalThread
