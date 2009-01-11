05ea7e20fb156ea9bf86d9a74320f3bf98eaf3a1
David M. Bryson <david@programerror.com>
2009-01-11 17:42:53 +0000

    Add general purpose analyze job batch class.  This class can be used to build asynchronous multipart jobs for execution in cAnalyzeJobQueue.   The advantage of such batches is that they can be submitted and completed regardless of other activity in the queue.   Once the queue becomes a true global resource this will be particularly important.

--- source/actions/LandscapeActions.cc
+++ source/actions/LandscapeActions.cc
@@ -28,7 +28,6 @@
 #include "cActionLibrary.h"
 #include "cAnalyze.h"
 #include "cAnalyzeGenotype.h"
-#include "tAnalyzeJob.h"
 #include "cClassificationManager.h"
 #include "cGenotype.h"
 #include "cGenotypeBatch.h"
@@ -45,7 +44,10 @@
 #include "cString.h"
 #include "cWorld.h"
 #include "cWorldDriver.h"
+#include "tAnalyzeJob.h"
+#include "tAnalyzeJobBatch.h"
 #include "tSmartArray.h"
+
 #include "defs.h"
 
 
@@ -186,12 +188,12 @@ public:
         m_world->GetDriver().NotifyComment("Precalculating landscape...");
       }
       
-      cAnalyzeJobQueue& jobqueue = m_world->GetAnalyze().GetJobQueue();      
+      tAnalyzeJobBatch<cAnalyzeGenotype> jobbatch(m_world->GetAnalyze().GetJobQueue());
       tListIterator<cAnalyzeGenotype> batch_it(m_world->GetAnalyze().GetCurrentBatch().List());
       for (cAnalyzeGenotype* cur_genotype = batch_it.Next(); cur_genotype; cur_genotype = batch_it.Next()) {
-        jobqueue.AddJob(new tAnalyzeJob<cAnalyzeGenotype>(cur_genotype, &cAnalyzeGenotype::CalcLandscape));
+        jobbatch.AddJob(cur_genotype, &cAnalyzeGenotype::CalcLandscape);
       }
-      jobqueue.Execute();
+      jobbatch.RunBatch();
     }
   }
 };
