86533338e5bc0694a597d83e0f1c06e37220203a
David M. Bryson <david@programerror.com>
2010-10-20 19:38:13 +0000

    Add stub cocoa app for building viewer-macos.

---
+++ source/targets/viewer-macos/AvidaAppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  AvidaAppDelegate.m
+//  Avida
+//
+//  Created by David Bryson on 10/20/10.
+//  Copyright 2010 Michigan State University. All rights reserved.
+//
+
+#import "AvidaAppDelegate.h"
+
+@implementation AvidaAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application 
+}
+
+@end
---
+++ source/targets/viewer-macos/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Avida
+//
+//  Created by David Bryson on 10/20/10.
+//  Copyright 2010 Michigan State University. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
