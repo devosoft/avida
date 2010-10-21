508838622d5c371eda30ec396a766ba7ae2f38f3
David M. Bryson <david@programerror.com>
2010-10-21 21:12:24 +0000

    Some basic tweaks to the viewer-macos nib.  Convert main file to .mm.  Add call to Avida::Initialize in main.  Link avida-core and viewer-core libraries.  Add stub MainWindowController.

---
+++ source/targets/MainWindowController.h
@@ -0,0 +1,18 @@
+//
+//  MainWindowController.h
+//  Avida
+//
+//  Created by David Bryson on 10/21/10.
+//  Copyright 2010 Michigan State University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class AvidaRun;
+
+@interface MainWindowController : NSObject {
+  AvidaRun* currentRun;
+}
+
+
+@end
---
+++ source/targets/MainWindowController.mm
@@ -0,0 +1,14 @@
+//
+//  MainWindowController.mm
+//  Avida
+//
+//  Created by David Bryson on 10/21/10.
+//  Copyright 2010 Michigan State University. All rights reserved.
+//
+
+#import "MainWindowController.h"
+
+
+@implementation MainWindowController
+
+@end
