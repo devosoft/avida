5c989fe6553555e404fb4995e95618f2415eb29e
David M. Bryson <david@programerror.com>
2011-03-22 22:49:09 -0400

    Update file comment headers for viewer-macos project.

--- src/viewer-core/MapGridView.h
+++ src/viewer-core/MapGridView.h
@@ -1,21 +1,30 @@
 //
 //  MapGridView.h
-//  Avida
+//  avida/apps/viewer-macos
 //
 //  Created by David on 11/23/10.
-//  Copyright 2010 Michigan State University. All rights reserved.
+//  Copyright 2010-2011 Michigan State University. All rights reserved.
+//  http://avida.devosoft.org/viewer-macos
 //
+//  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+//  following conditions are met:
+//  
+//  1.  Redistributions of source code must retain the above copyright notice, this list of conditions and the
+//      following disclaimer.
+//  2.  Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+//      following disclaimer in the documentation and/or other materials provided with the distribution.
+//  3.  Neither the name of Michigan State University, nor the names of contributors may be used to endorse or promote
+//      products derived from this software without specific prior written permission.
+//  
+//  THIS SOFTWARE IS PROVIDED BY MICHIGAN STATE UNIVERSITY AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+//  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL MICHIGAN STATE UNIVERSITY OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+//  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+//  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+//  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+//  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
-//  This file is part of Avida.
-//
-//  Avida is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License
-//  as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-//
-//  Avida is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
-//
-//  You should have received a copy of the GNU Lesser General Public License along with Avida.
-//  If not, see <http://www.gnu.org/licenses/>.
+//  Authors: David M. Bryson <david@programerror.com>
 //
 
 #import <Cocoa/Cocoa.h>
