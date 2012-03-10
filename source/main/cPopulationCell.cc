0322843f6286a39a5d1b9f67235e2c6ac6aad35a
Aaron Wagner <apwagner@msu.edu>
2012-03-10 17:11:08 -0500

    avatar-pred test changed to use different seed-prev seed left one org doing nothing (by chance) Corrected error in av index handling from last commit.

--- source/main/cPopulationCell.cc
+++ source/main/cPopulationCell.cc
@@ -312,11 +312,9 @@ void cPopulationCell::AddInputAV(cOrganism* org)
   // Swaps the added avatar into a random position in the array
   int loc = m_world->GetRandom().GetUInt(0, m_av_inputs.GetSize());
   cOrganism* exist_org = m_av_inputs[loc];
-  if (exist_org != org) {
-    m_av_inputs.Swap(loc, m_av_inputs.GetSize() - 1);
-    exist_org->SetAVInIndex(m_av_inputs.GetSize() - 1);
-    org->SetAVInIndex(loc);
-  }
+  m_av_inputs.Swap(loc, m_av_inputs.GetSize() - 1);
+  exist_org->SetAVInIndex(m_av_inputs.GetSize() - 1);
+  org->SetAVInIndex(loc);
 }
 
 // Adds an organism to the cell's output avatars, then keeps the list mixed by swapping the new avatar into a random position in the array
@@ -326,11 +324,9 @@ void cPopulationCell::AddOutputAV(cOrganism* org)
   // Swaps the added avatar into a random position in the array
   int loc = m_world->GetRandom().GetUInt(0, m_av_outputs.GetSize());
   cOrganism* exist_org = m_av_outputs[loc];
-  if (exist_org != org) {
-    m_av_outputs.Swap(loc, m_av_outputs.GetSize() - 1);
-    exist_org->SetAVOutIndex(m_av_outputs.GetSize() - 1);
-    org->SetAVOutIndex(loc);
-  }
+  m_av_outputs.Swap(loc, m_av_outputs.GetSize() - 1);
+  exist_org->SetAVOutIndex(m_av_outputs.GetSize() - 1);
+  org->SetAVOutIndex(loc);
 }
 
 // Removes the organism from the cell's input avatars
