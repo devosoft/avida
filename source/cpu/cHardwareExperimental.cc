aa6a2b4ee1e5eafeebfa0f2664a1901d640bd7f4
Aaron Wagner <apwagner@msu.edu>
2012-11-04 12:34:07 -0500

    Additions to prev inst.

--- source/cpu/cHardwareExperimental.cc
+++ source/cpu/cHardwareExperimental.cc
@@ -4132,27 +4132,33 @@ bool cHardwareExperimental::Inst_SetForageTargetOnce(cAvidaContext& ctx)
 bool cHardwareExperimental::Inst_SetRandForageTargetOnce(cAvidaContext& ctx)
 {
   assert(m_organism != 0);
-  if (m_organism->HasSetFT()) return false;
-  else {
-    int num_fts = 0;
-    std::set<int> fts_avail = m_world->GetEnvironment().GetTargetIDs();
-    set <int>::iterator itr;
-    for (itr = fts_avail.begin();itr!=fts_avail.end();itr++) if (*itr != -1 && *itr != -2 && *itr != -3) num_fts++;
-    int prop_target = m_world->GetRandom().GetUInt(num_fts);
-    if (m_world->GetEnvironment().IsTargetID(-1) && num_fts == 0) prop_target = -1;
+  int cap = 0;
+  if (m_world->GetConfig().POPULATION_CAP.Get()) cap = m_world->GetConfig().POPULATION_CAP.Get();
+  else if (m_world->GetConfig().POP_CAP_ELDEST.Get()) cap = m_world->GetConfig().POP_CAP_ELDEST.Get();
+  if (cap && (m_organism->GetOrgInterface().GetLiveOrgList().GetSize() >= (((double)(cap)) * 0.5))) {
+    if (m_organism->HasSetFT()) return false;
     else {
-      // ft's may not be sequentially numbered
-      int ft_num = abs(prop_target) % num_fts;
-      itr = fts_avail.begin();
-      for (int i = 0; i < ft_num; i++) itr++;
-      prop_target = *itr;
+      int num_fts = 0;
+      std::set<int> fts_avail = m_world->GetEnvironment().GetTargetIDs();
+      set <int>::iterator itr;
+      for (itr = fts_avail.begin();itr!=fts_avail.end();itr++) if (*itr != -1 && *itr != -2 && *itr != -3) num_fts++;
+      int prop_target = m_world->GetRandom().GetUInt(num_fts);
+      if (m_world->GetEnvironment().IsTargetID(-1) && num_fts == 0) prop_target = -1;
+      else {
+        // ft's may not be sequentially numbered
+        int ft_num = abs(prop_target) % num_fts;
+        itr = fts_avail.begin();
+        for (int i = 0; i < ft_num; i++) itr++;
+        prop_target = *itr;
+      }
+      // Set the new target and return the value
+      m_organism->SetForageTarget(prop_target);
+      m_organism->RecordFTSet();
+      setInternalValue(FindModifiedRegister(rBX), prop_target, false);
+      return true;
     }
-    // Set the new target and return the value
-    m_organism->SetForageTarget(prop_target);
-    m_organism->RecordFTSet();
-    setInternalValue(FindModifiedRegister(rBX), prop_target, false);
-    return true;
   }
+  else return Inst_SetForageTargetOnce(ctx);
 }
 
 bool cHardwareExperimental::Inst_GetForageTarget(cAvidaContext& ctx)
