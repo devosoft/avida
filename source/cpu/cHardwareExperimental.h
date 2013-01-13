d5113d861c5b79002cbcee1b7c7e93c226ae4a7d
Aaron Wagner <apwagner@msu.edu>
2013-01-13 10:57:40 -0500

    Updating predator 'group' hunting instructions.

--- source/cpu/cHardwareExperimental.h
+++ source/cpu/cHardwareExperimental.h
@@ -627,6 +627,7 @@ private:
   bool Inst_AttackPrey(cAvidaContext& ctx); 
   bool Inst_AttackPreyGroup(cAvidaContext& ctx);
   bool Inst_AttackPreyShare(cAvidaContext& ctx);
+  bool Inst_AttackPreyGroupShare(cAvidaContext& ctx);
   bool Inst_AttackSpecPrey(cAvidaContext& ctx);
   bool Inst_AttackPreyArea(cAvidaContext& ctx);
   bool Inst_AttackFTPrey(cAvidaContext& ctx); 
