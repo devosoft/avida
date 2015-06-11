642d00cd3b1c6a4292b3abffd9ac32c4f4b9db32
Matthew R. Rupp <ruppmatt@gmail.com>
2015-06-11 13:08:04 -0400

    Initial stub for web interface.  (Not working.)

---
+++ apps/viewer-web/src/viewer-web.cc
@@ -0,0 +1,23 @@
+#include "avida/Avida.h"
+#include "AvidaTools.h"
+
+#include "cAvidaConfig.h"
+#include "cDriverManager.h"
+#include "cUserFeedback.h"
+#include "cWorld.h"
+
+#include "apto/
+
+
+
+
+int main(int argc, char* argv[])
+{
+
+  //Initialize Avida global objects
+  Avida::Initialize();
+  
+  //Initialize configuration data
+  auto cfg = Apto::SmartPtr(
+  
+}
